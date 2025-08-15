09420bd574d3c289b4cb5465f5a14c9dc7728348
Muhammad Sohail Hafeez <sohailhafeez6464@gmail.com>
2025-08-15 11:30:41 +0500

    Update student.py
    Added Exceptions with status code and path for describing so that clients can understand easily by simply looking at the documentation
    for documentation simply hit at /docs endpoint

--- First Simple get fast API/Viewing the data from json file/student.py
+++ First Simple get fast API/Viewing the data from json file/student.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Path,HTTPException
 from pydantic import BaseModel
 import json
 
@@ -23,14 +23,18 @@ def data():
     return data 
 
 @app.get("/student/{student_id}")
-def viewStudent(student_id: str):
+def viewStudent(student_id: str  = Path(..., description = "Enter the number of student" , example = 'student1' )):
     data = loadData()
     if student_id in data:
         return data[student_id]
+    raise HTTPException(status_code=404 , detail="Student not found")
     
 @app.get("/students/{student_id}/{attribute}")
-def viewStudent(student_id: str, attribute: str):
+def viewStudent(student_id: str = Path(..., description = "Enter the number of student" , example = 'student1' ), attribute: str= Path(..., description = "Enter the number of attribute" , example = 'id/name and .....' )):
     data = loadData()
     if student_id in data:
         if attribute in data[student_id]:
             return {attribute: data[student_id][attribute]}
+        raise HTTPException(status_code=404 , detail=f"No attribute of {attribute} in {student_id}")
+    raise HTTPException(status_code=404 , detail=f"No student as {student_id} found")
+    
