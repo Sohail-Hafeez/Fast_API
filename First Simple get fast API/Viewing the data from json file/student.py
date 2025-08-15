f274466f8522b259650a9529f5b35099bd5bdf79
Muhammad Sohail Hafeez <sohailhafeez6464@gmail.com>
2025-08-15 12:34:23 +0500

    Updated Query in  student.py
    Added Queries to sort with respect to any attribute and you can order in Ascending or Descending

--- First Simple get fast API/Viewing the data from json file/student.py
+++ First Simple get fast API/Viewing the data from json file/student.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path,HTTPException
+from fastapi import FastAPI, Path,HTTPException , Query
 from pydantic import BaseModel
 import json
 
@@ -11,7 +11,7 @@ app = FastAPI()
 
 @app.get("/")
 def Title():
-    return {"message" : "MCS student MAnagement System"}
+    return {"message" : "MCS student Management System"}
 
 @app.get("/about")
 def about():
@@ -23,18 +23,38 @@ def data():
     return data 
 
 @app.get("/student/{student_id}")
-def viewStudent(student_id: str  = Path(..., description = "Enter the number of student" , example = 'student1' )):
+def viewStudent(student_id: str  = Path(..., description = "Enter the number of student" , examples = 'student1' )):
     data = loadData()
     if student_id in data:
         return data[student_id]
     raise HTTPException(status_code=404 , detail="Student not found")
+ 
+ 
     
 @app.get("/students/{student_id}/{attribute}")
-def viewStudent(student_id: str = Path(..., description = "Enter the number of student" , example = 'student1' ), attribute: str= Path(..., description = "Enter the number of attribute" , example = 'id/name and .....' )):
+def viewStudent(student_id: str = Path(..., description = "Enter the number of student" , examples = 'student1' ), attribute: str= Path(..., description = "Enter the number of attribute" , example = 'id/name and .....' )):
     data = loadData()
     if student_id in data:
         if attribute in data[student_id]:
             return {attribute: data[student_id][attribute]}
         raise HTTPException(status_code=404 , detail=f"No attribute of {attribute} in {student_id}")
     raise HTTPException(status_code=404 , detail=f"No student as {student_id} found")
+
+
+
     
+@app.get('/sort')
+def sortStudents(sort_by: str = Query(..., description= "Sort on the bases of id , CGPA"), order: str = Query(description="Sort in asc or desc order")):
+    field = ['id' , "CGPA" , "projects"]
+    if sort_by not in field:
+        raise HTTPException(status_code=400 , detail=f"No method of sorting such as {sort_by} exists")
+    if order not in ['asc' , 'desc']:
+        raise HTTPException(status_code=400 , detail=f"inorder order selection kindly select asc or desc")
+    
+    data = loadData()
+    
+    sortData = True if order=='desc' else False
+    
+    sortedData = sorted(data.values() , key = lambda x: x.get(sort_by , 0), reverse = sortData )
+    
+    return sortedData
