b3b430049806c42c6d8be5f6046115b3fc38fb73
Muhammad Sohail Hafeez <sohailhafeez6464@gmail.com>
2025-08-17 10:00:06 +0500

    Update app.py
    Added field_validator
    
    Added 2 functions under field validator
    1. For Email 
    2. For name

--- pydantic/app.py
+++ pydantic/app.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, EmailStr, Field, ValidationError
+from pydantic import BaseModel, EmailStr, Field, ValidationError , field_validator
 from typing import List, Dict, Optional , Annotated
 
 class Student(BaseModel):
@@ -9,10 +9,32 @@ class Student(BaseModel):
     gender: Annotated[str , Field(default='Male', description = 'Please write the gender of student') ]
     allergies: Annotated[Optional[List[str]] , Field(default=None , max_length = 5 )]
     contactDetail: Dict[str, str]
+    
+    # Concept of field Validator 
+    # If you want to make Custom Email validator or some thing Similar so you have to maek a field VAlidator 
 
+    @field_validator('email')
+    @classmethod 
+    def emailValidator(cls , value):
+        validDomain = ['pakistan.com' , 'nust.com']
+        domainName = value.split('@')[-1]
+        
+        if domainName not in validDomain:
+            raise ValueError("Not a Valid Domain")
+        return value
+    
+    
+    # using a field validator for name 
+    @field_validator('name')
+    @classmethod
+    def namevalidator(cls,value):
+        return value.upper()
+        
+    
+    
 studentData = {
     "name": "Sohail",
-    "email": "sohailhafeez6464@gmail.com", 
+    "email": "sohailhafeez6464@pakistan.com", 
     "age": 20,
     "weight": 85.8492,  
     "gender": "Male",
@@ -20,9 +42,12 @@ studentData = {
     "contactDetail": {"number": "03473073674"}
 }
 
+
+
 try:
     student = Student(**studentData)
     print("Validation passed " )
+    print(student.name)
 except ValidationError as e:
     print("Validation failed ")
     print(e)
